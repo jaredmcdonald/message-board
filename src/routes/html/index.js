5ed36044d6d792ee25ee2adc57f5a0de794fa71b
Jared McDonald <jared.r.mcdonald@gmail.com>
2015-03-21 15:55:42 -0600

    make app name configurable

--- src/routes/html/index.js
+++ src/routes/html/index.js
@@ -1,5 +1,7 @@
 let router = require('express').Router();
 
-router.get('/', (req, res) => res.render('index', { title : 'express-board' }));
+router.get('/', (req, res) => res.render('index', {
+    title : process.env.APPLICATION_NAME || 'express-board'
+  }));
 
 module.exports = router;
